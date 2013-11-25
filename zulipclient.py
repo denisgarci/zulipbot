7a0800f41b28f4873063ff14e934d40a4988cc37
Denis Garcia <denisgarci@gmail.com>
2013-11-25 12:23:49 -0500

    Support for sending message

---
+++ zulipclient.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import requests
+
+
+class ZulipCient(object):
+
+    zulip_api_url = "https://api.zulip.com/v1/messages"
+
+    def __init__(self, email, api_key):
+        self.email = email
+        self.api_key = api_key
+        self.bot_auth = (self.email, self.api_key)
+
+    def send_message(self, **kwargs):
+        # type is a python keyword
+        kwargs["type"] = kwargs.pop("message_type")
+        print(kwargs)
+        r = requests.post(self.zulip_api_url, params=kwargs, auth=self.bot_auth)
+        resp = r.json()
+        print(resp["result"])
+        # replace that with a function and a try? if "result" not in rep...
+        if resp["result"] == "success": # we should use a try
+            print("Message sent", kwargs)
+        else:
+            print("Send message error")
+            print(resp)
+
+    def subscribe_to_streams(self, streams):
+        streams = str(streams).replace("'", '"') # must use double quotes in interior of list
+        assert streams.startswith('["')
+        params = {"add" : streams}
+        r = requests.patch(self.zulip_api_url, params=params, auth=self.bot_auth)
+        resp = r.json()
+        if resp["result"] == "success":
+            print("Subscribed to streams", streams)
+        else:
+            print("Subscription error")
+            print(resp)
+
+
+if __name__ == '__main__':
+    email = 'babar-bot@students.hackerschool.com'
+    api_key = '9OKMWIbzeMfz7cI33UFqWvZ7vYFzNo84'
+
+    client = ZulipCient(email, api_key)
+    client.send_message(message_type="private",\
+                        to="denisgarci@gmail.com",\
+                        content="Salut c'est Babar.")
+
+
