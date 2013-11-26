e7c9af43d9d71a831dd6460341456ffbc1db4795
Denis Garcia <denisgarci@gmail.com>
2013-11-26 11:28:01 -0500

    Added get_event and register_queue, bot can receive messages!

--- zulipclient.py
+++ zulipclient.py
@@ -6,7 +6,12 @@ import requests
 
 class ZulipCient(object):
 
-    zulip_api_url = "https://api.zulip.com/v1/messages"
+    zulip_api_url = "https://api.zulip.com/v1/{0}"
+    zulip_api_message = zulip_api_url.format("messages")
+    zulip_api_register = zulip_api_url.format("register")
+    zulip_api_events = zulip_api_url.format("events")
+
+    event_types = {"message", "subscriptions", "realm_user", "pointer"}
 
     def __init__(self, email, api_key):
         self.email = email
@@ -17,7 +22,7 @@ class ZulipCient(object):
         # type is a python keyword
         kwargs["type"] = kwargs.pop("message_type")
         print(kwargs)
-        r = requests.post(self.zulip_api_url, params=kwargs, auth=self.bot_auth)
+        r = requests.post(self.zulip_api_message, params=kwargs, auth=self.bot_auth)
         resp = r.json()
         print(resp["result"])
         # replace that with a function and a try? if "result" not in rep...
@@ -39,6 +44,24 @@ class ZulipCient(object):
             print("Subscription error")
             print(resp)
 
+    def register_queue(self, *events):
+        assert self.event_type.issuperset(set(events))
+        events = str(events).replace("'", '"') # must use double quotes in interior of list
+        assert events.startswith('["')
+        params = {"event_types": events}
+        r = requests.post(self.zulip_api_register, params=params, auth=self.bot_auth)
+        resp = r.json()
+        if resp["result"] == "success": # we should use a try
+            print("Regisered to ", events)
+        else:
+            print("Send message error")
+            print(resp)
+
+    def get_event(self, queue_id):
+        params = {"queue_id": queue_id,  "last_event_id": -1}
+        r = requests.get(self.zulip_api_events, params=params, auth=self.bot_auth)
+        resp = r.json()
+        return resp
 
 if __name__ == '__main__':
 
@@ -48,8 +71,12 @@ if __name__ == '__main__':
     api_key = environ['zulip_key']
 
     client = ZulipCient(email, api_key)
-    client.send_message(message_type="private",\
-                        to="denisgarci@gmail.com",\
-                        content="Salut c'est Babar.")
+    r = client.register_queue('message')
+    print(r)
+    queue_id = r['queue_id']
+    print(client.get_event(queue_id))
+    #client.send_message(message_type="private",\
+                    #to="allison@hackerschool.com",\
+                    #content="Yes")
 
 
